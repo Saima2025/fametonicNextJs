2bde9f71211ae9059ba1d52ae3db96229bb542f5
Saima_TL <saima@trucklagbe.com>
2025-04-25 17:19:30 +0600

    # mobile screen fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Image from "next/image";
 export default function Home() {
     return (
         <>
-            <div className="bg-neutral-950 sm:flex md:px-15 md:flex-row sm:static md:h-svh">
-                <div className="md:flex md:flex-col sm:z-20 md:z-2 sm:min-w-screen md:min-w-md sm:mt-160 md:mt-0 sm:px-15 md:relative sm:static">
-                    <p className="text-4xl text-white font-bold sm:text-center md:text-left">Want to Turn Social Media Into a Profitable Career?</p>
+            <div className="bg-neutral-950 sm:flex max-sm:flex lg:px-15 lg:flex-row sm:static max-sm:static lg:h-svh">
+                <div
+                    className="lg:flex lg:flex-col sm:z-20 max-sm:z-20 lg:z-2 sm:min-w-screen max-sm:min-w-screen lg:min-w-lg sm:mt-[86%] md:mt-[90%] max-sm:mt-[84%] lg:mt-[1%] sm:px-15 max-sm:px-15 lg:relative sm:static max-sm:static">
+                    <p className="text-4xl text-white font-bold sm:text-center max-sm:text-center lg:text-left">Want to
+                        Turn Social Media Into a Profitable Career?</p>
                     <p className="text-4xl text-cyan-400 text-shadow-lg/50 [text-shadow:_0_4px_8px_#FC004E] text-[#FC004E] font-bold mt-3
-                     sm:text-center md:text-left">Discover your way to success with Fametonic:</p>
-                    <p className="text-base  text-white font-bold inline-flex items-baseline flex items-center md:mt-3 sm:mt-8">
+                     sm:text-center max-sm:text-center lg:text-left">Discover your way to success with Fametonic:</p>
+                    <p className="text-base  text-white font-bold inline-flex items-baseline flex items-center lg:mt-3 sm:mt-8 max-sm:mt-8">
                         <Image
                             src="/home/bulletPoint.png"
                             alt="Picture of the author"
@@ -40,9 +42,17 @@ export default function Home() {
                             height={20}
                             className="mr-2"
                         /> Learn from expert-led courses designed for aspiring influencers</p>
-                    <div className="md:w-1/2 sm:mt-20 md:mt-0 sm:place-content-center sm:grid md:place-content-start md:flex md:flex-col">
+                    <div
+                        className="relative lg:invisible sm:visible max-sm:visible sm:text-center max-sm:text-center">
+                        <p className="text-xs text-gray-400 mt-6">By clicking "Get Started", you agree with Terms and
+                            Conditions, Privacy Policy, Subscription Terms </p>
+                        <p className="text-[10px] text-gray-400 mt-5">Fametonic 2025 ©All Rights Reserved.</p>
+                    </div>
+                    <div
+                        className="lg:w-1/2 sm:place-content-center max-sm:place-content-center sm:grid max-sm:grid lg:place-content-start lg:flex lg:flex-col">
                         <button type="button"
-                                className="inline-flex justify-center md:w-full sm:w-lg mt-3 text-white bg-pink-600 focus:ring-2 focus:outline-none focus:ring-cyan-300 shadow-lg shadow-cyan-500/50 font-bold rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2 uppercase ">Get Started
+                                className="inline-flex justify-center lg:w-full sm:w-lg max-sm:w-md mt-3 text-white bg-pink-600 focus:ring-2 focus:outline-none focus:ring-cyan-300 shadow-lg shadow-cyan-500/50 font-bold rounded-lg text-xl px-5 py-2.5 text-center me-2 mb-2 uppercase ">Get
+                            Started
                             <Image
                                 src="/home/forwardIcon.svg"
                                 alt="Picture of the author"
@@ -51,15 +61,18 @@ export default function Home() {
                                 height={15}
                             />
                         </button>
-                        <p className="text-xs text-white mt-2 flex justify-center mb-8">1-minute quiz for personalized Insights</p>
+                        <p className="text-xs text-white mt-2 flex justify-center mb-8">1-minute quiz for personalized
+                            Insights</p>
                     </div>
-                    <div className="md:relative sm:absolute sm:top-332 md:top-0 sm:text-center md:text-left sm:pl-8 md:pl-0">
-                        <p className="text-xs text-gray-400 mt-6">By clicking "Get Started", you agree with Terms and Conditions, Privacy Policy, Subscription Terms </p>
+                    <div
+                        className="relative lg:visible sm:invisible max-sm:invisible lg:text-left ">
+                        <p className="text-xs text-gray-400 mt-6">By clicking "Get Started", you agree with Terms and
+                            Conditions, Privacy Policy, Subscription Terms </p>
                         <p className="text-[10px] text-gray-400 mt-5">Fametonic 2025 ©All Rights Reserved.</p>
                     </div>
                 </div>
 
-                <div className="absolute md:top-40 sm:top-80 sm:px-15 md:left-200 ">
+                <div className="absolute lg:top-40 sm:top-80 max-sm:top-80 sm:px-15 max-sm:px-15 lg:left-200">
                     <Image
                         src="/home/homeImage.svg"
                         alt="Picture of the author"
@@ -72,4 +85,4 @@ export default function Home() {
             </div>
         </>
     );
-}//z-40 absolute top-90
+}
